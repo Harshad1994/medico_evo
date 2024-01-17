8d5fb282d239a5e6ab8c60375c5599688bb17d4d
Harshad1994 <harshadkunjir@gmail.com>
2024-01-17 16:55:40 +0530

    Create main.py

---
+++ tunedBio_ClinicalBERT/app/main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+import uuid
+from pydantic import BaseModel
+from backend import predict
+
+
+class InputRequest(BaseModel):
+    abstract: str
+
+class ResponseModel(BaseModel):
+    res_id: str
+    category: str
+    confidence: float
+
+app = FastAPI()
+
+@app.post("/classify")
+def classify(request_json: InputRequest) -> ResponseModel | dict:
+ 
+    print("hitting the endpoint")
+
+    res_id=str(uuid.uuid4())
+
+    text = request_json.abstract
+
+    prediction,prob = predict(text)
+
+    response_dict = {"res_id":res_id,"category":prediction,"confidence":prob}
+
+    return ResponseModel(**response_dict)
+
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)

9462e751cdb53c5cd0a52c4323fadc57b65a62d6
Harshad1994 <harshadkunjir@gmail.com>
2024-01-17 18:18:08 +0530

    Create main_mistral.py

---
+++ peft_Mistral7/app/main_mistral.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI
+import uuid
+from pydantic import BaseModel
+from backend_mistral import predict
+
+
+class InputRequest(BaseModel):
+    abstract: str
+
+class ResponseModel(BaseModel):
+    res_id: str
+    category: str
+    category_index: int | None
+    confidence: float
+
+app = FastAPI()
+
+@app.post("/generate")
+def classify(request_json: InputRequest) -> ResponseModel | dict:
+ 
+    print("hitting the endpoint")
+
+    res_id=str(uuid.uuid4())
+
+    text = request_json.abstract
+
+    prediction,cat_index,prob = predict(text)
+
+    response_dict = {"res_id":res_id,"category":prediction,"category_index":cat_index,"confidence":prob}
+
+    return ResponseModel(**response_dict)
+
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8081)
